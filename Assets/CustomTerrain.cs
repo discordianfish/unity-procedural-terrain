f3efaee6df2c9f254da81dc9850dc07bd1be86e8
Johannes 'fish' Ziemke <github@freigeist.org>
2018-05-23 16:07:02 +0200

    Add CustomTerrain scripts

---
+++ Assets/CustomTerrain.cs
@@ -0,0 +1,475 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+[ExecuteInEditMode]
+
+public class CustomTerrain : MonoBehaviour {
+    public Vector2 randomHeightRange = new Vector2(0, 0.1f);
+    public Texture2D heightMapImage;
+    public Vector3 heightMapScale = new Vector3(1, 1, 1);
+
+    public bool resetTerrain = true;
+
+    //SINGLE PERLIN NOISE --------------
+    public float perlinXScale = 0.01f;
+    public float perlinYScale = 0.01f;
+
+    public int perlinXOffset = 0;
+    public int perlinYOffset = 0;
+    public int perlinOctaves = 0;
+    public float perlinPersistance = 8;
+    public float perlinHeightScale = 0.09f;
+
+    //MULTIPLE PERLIN NOISE --------------
+    [System.Serializable]
+    public class PerlinParameters
+    {
+        public float XScale = 0.01f;
+        public float YScale = 0.01f;
+
+        public int XOffset = 0;
+        public int YOffset = 0;
+        public int octaves = 0;
+        public float persistance = 8;
+        public float heightScale = 0.09f;
+        public bool remove = false;
+    }
+    public List<PerlinParameters> perlinParameters = new List<PerlinParameters>()
+    {
+        new PerlinParameters()
+    };
+
+
+    // VORONOI
+    public int voronoiPeaks = 5;
+    public float voronoiFallOff = 0.2f;
+    public float voronoiDropOff = 0.6f;
+    public float voronoiMinHeight = 0.1f;
+    public float voronoiMaxHeight = 0.5f;
+    public enum VoronoiType {  Linear = 0, Power = 1, Combined = 2, Round = 3};
+    public VoronoiType voronoiType = VoronoiType.Linear;
+
+    // MPD
+    public float mpdMinHeight = -10f;
+    public float mpdMaxHeight = 10f;
+    public float mpdDampenerPower = 2f;
+    public float mpdRoughness = 2f;
+
+    // Smooth
+    public int smoothAmount = 1;
+    public Terrain terrain;
+    public TerrainData terrainData;
+
+    float[,]  GetHeightMap()
+    {
+        if (resetTerrain)
+        {
+            return new float[terrainData.heightmapWidth, terrainData.heightmapHeight];
+        }
+        return terrainData.GetHeights(0, 0, terrainData.heightmapWidth, terrainData.heightmapHeight);
+    }
+
+    List<Vector2> GenerateNeighbours(Vector2 pos, int width, int height)
+    {
+        List<Vector2> neighbours = new List<Vector2>();
+        for (int y = -1; y < 2; y++)
+        {
+            for (int x = -1; x < 2; x++)
+            {
+                if (x == 0 && y == 0)
+                    continue;
+                Vector2 nPos = new Vector2(Mathf.Clamp(pos.x + x, 0, width - 1),
+                    Mathf.Clamp(pos.y + y, 0, height - 1));
+
+                if (!neighbours.Contains(nPos))
+                    neighbours.Add(nPos);
+            }
+        }
+        return neighbours;
+    }
+    public void SmoothN()
+    {
+        for (int n = 0; n < smoothAmount; n++)
+        {
+            EditorUtility.DisplayProgressBar("Smoothing Terrain", "Progress", n / smoothAmount);
+            Smooth();
+        }
+        EditorUtility.ClearProgressBar();
+    }
+    public void Smooth()
+    {
+        float[,] heightMap = terrainData.GetHeights(0, 0, terrainData.heightmapWidth, terrainData.heightmapHeight);
+
+        for (int x = 0; x < terrainData.heightmapWidth; x++)
+        {
+            for (int y = 0; y < terrainData.heightmapHeight; y++)
+            {
+                float avgHeight = heightMap[x, y];
+                List<Vector2> neighbours = GenerateNeighbours(new Vector2(x, y),
+                    terrainData.heightmapWidth,
+                    terrainData.heightmapHeight);
+
+                foreach (Vector2 n in neighbours)
+                {
+                    avgHeight += heightMap[(int)n.x, (int)n.y];
+                }
+                heightMap[x, y] = avgHeight / ((float)neighbours.Count + 1);
+            }
+        }
+        terrainData.SetHeights(0, 0, heightMap);
+    }
+
+
+    public void SmoothMy()
+    {
+        float[,] heightMap = GetHeightMap();
+
+        for (int x = 0; x < terrainData.heightmapWidth; x++)
+        {
+            for (int y = 0; y < terrainData.heightmapHeight; y++)
+            {
+                int div = 0;
+                float sum = 0;
+                for (int xi = x-1; xi <= x+1; xi++)
+                {
+                    if (xi < 0 || xi >= terrainData.heightmapWidth)
+                        continue;
+
+                    for (int yi = y-1; yi <= y+1; yi++)
+                    {
+                        if (yi < 0 || yi >= terrainData.heightmapHeight)
+                            continue;
+
+                        div++;
+                        sum += heightMap[xi, yi];
+                    }
+                }
+                // Debug.Log("x = " + x + ", y = " + y + ", sum = " + sum + ", div = " + div);
+                heightMap[x, y] = sum / div;
+                /*
+                        heightMap[x, y] = (heightMap[x, y] +
+                                           heightMap[x + 1, y] +
+                                           heightMap[x - 1, y] +
+                                           heightMap[x + 1, y + 1] +
+                                           heightMap[x - 1, y - 1] +
+                                           heightMap[x + 1, y - 1] +
+                                           heightMap[x - 1, y + 1] +
+                                           heightMap[x, y + 1] +
+                                           heightMap[x, y - 1]) / 9.0f;*/
+            }
+        }
+        terrainData.SetHeights(0, 0, heightMap);
+    }
+
+    public void MidPointDisplacement()
+    {
+        float[,] heightMap = GetHeightMap();
+        int width = terrainData.heightmapWidth - 1;
+        int squareSize = width;
+        float heightMin = mpdMinHeight;
+        float heightMax = mpdMaxHeight;
+
+        // float height = (float)squareSize / 2.0f * 0.01f;
+        float heightDampener = (float)Mathf.Pow(mpdDampenerPower, -1 * mpdRoughness);
+
+        int cornerX, cornerY;
+        int midX, midY;
+        // (x, cornerY)   (cornerX, cornerY)
+        // +-------------------------------+
+        // |                               |
+        // |             x (midX, midY)    |
+        // |                               |
+        // +-------------------------------+
+        // (x, y)               (cornerX, y)
+
+        int pmidXL, pmidXR, pmidYU, pmidYD;
+
+        // Add random corner heights
+        /*
+        heightMap[0, 0] = UnityEngine.Random.Range(0f, 0.2f);
+        heightMap[0, terrainData.heightmapHeight - 2] = UnityEngine.Random.Range(0f, 0.2f);
+        heightMap[terrainData.heightmapWidth - 2, 0] = UnityEngine.Random.Range(0f, 0.2f);
+        heightMap[terrainData.heightmapWidth - 2, terrainData.heightmapHeight - 2] =
+            UnityEngine.Random.Range(0f, 0.2f);*/
+        // - 2 because heightMap is mesh size + 1
+
+        while (squareSize > 0)
+        {
+            Debug.Log("squareSize = " + squareSize);
+            for (int x = 0; x < width; x += squareSize)
+            {
+                for (int y = 0; y < width; y += squareSize)
+                {
+                    cornerX = (x + squareSize);
+                    cornerY = (y + squareSize);
+                    midX = (int)(x + squareSize / 2.0f);
+                    midY = (int)(y + squareSize / 2.0f);
+
+                    // Set mid point to average of corner heights
+                    heightMap[midX, midY] = (float)((heightMap[x, y] +
+                                                    heightMap[cornerX, y] +
+                                                    heightMap[x, cornerY] +
+                                                    heightMap[cornerX, cornerY]) / 4.0f + 
+                                                    UnityEngine.Random.Range(heightMin, heightMax));
+                }
+            }
+
+            for (int x = 0; x < width; x += squareSize)
+            {
+                for (int y = 0; y < width; y += squareSize)
+                {
+                    cornerX = (x + squareSize);
+                    cornerY = (y + squareSize);
+                    midX = (int)(x + squareSize / 2.0f);
+                    midY = (int)(y + squareSize / 2.0f);
+
+                    pmidXR = (int)(midX + squareSize);
+                    pmidYU = (int)(midY + squareSize);
+                    pmidXL = (int)(midX - squareSize);
+                    pmidYD = (int)(midY - squareSize);
+
+                    if (pmidXL <= 0 || pmidYD <= 0
+                        || pmidXR >= width - 1 || pmidYU >= width - 1) continue;
+
+                    heightMap[midX, y] = (float)((heightMap[x, y] +
+                                                  heightMap[midX, midY] +
+                                                  heightMap[cornerX, y] +
+                                                  heightMap[midX, pmidYD]) / 4.0f +
+                                                  UnityEngine.Random.Range(heightMin, heightMax));
+
+                    heightMap[midX, cornerY] = (float)((heightMap[x, cornerY] +
+                                                        heightMap[midX, pmidYU] +
+                                                        heightMap[cornerX, cornerY] +
+                                                        heightMap[midX, midY]) / 4.0f +
+                                                        UnityEngine.Random.Range(heightMin, heightMax));
+
+                    heightMap[x, midY] = (float)((heightMap[pmidXL, midY] +
+                                                  heightMap[x, cornerY] +
+                                                  heightMap[midX, midY] +
+                                                  heightMap[x, y]) / 4.0f +
+                                                  UnityEngine.Random.Range(heightMin, heightMax));
+
+                    heightMap[cornerX, midY] = (float)((heightMap[midX, midY] +
+                                                        heightMap[cornerX, cornerY] +
+                                                        heightMap[pmidXR, midY] +
+                                                        heightMap[cornerX, y]) / 4.0f +
+                                                        UnityEngine.Random.Range(heightMin, heightMax));
+                }
+            }
+
+            squareSize = (int)(squareSize / 2.0f);
+            heightMin *= heightDampener;
+            heightMax *= heightDampener;
+        }
+        terrainData.SetHeights(0, 0, heightMap);
+    }
+    public void Voronoi()
+    {
+        float[,] heightMap = GetHeightMap();
+        float maxDistance = Vector2.Distance(new Vector2(0, 0), new Vector2(terrainData.heightmapWidth, terrainData.heightmapHeight));
+
+        for (int i = 0; i < voronoiPeaks; i++)
+        {   
+            Vector3 peak = new Vector3(UnityEngine.Random.Range(0, terrainData.heightmapWidth),
+                                       UnityEngine.Random.Range(voronoiMinHeight, voronoiMaxHeight),
+                                       UnityEngine.Random.Range(0, terrainData.heightmapHeight)
+                                      );
+
+            if (heightMap[(int)peak.x, (int)peak.y] < peak.y)
+                heightMap[(int)peak.x, (int)peak.z] = peak.y;
+            else
+                continue;
+
+            Vector2 peakLocation = new Vector2(peak.x, peak.z);
+
+            for (int x = 0; x < terrainData.heightmapWidth; x++)
+            {
+                for (int y = 0; y < terrainData.heightmapHeight; y++)
+                {
+                    if (!(x == peak.x && y == peak.z))
+                    {
+                        float distanceToPeak = Vector2.Distance(peakLocation, new Vector2(x, y)) / maxDistance;
+                        float h = 0;
+                        
+                        switch(voronoiType)
+                        {
+                            case VoronoiType.Combined:
+                                h = peak.y - distanceToPeak * voronoiFallOff -
+                                    Mathf.Pow(distanceToPeak, voronoiDropOff);
+                                break;
+                            case VoronoiType.Power:
+                                h = peak.y - Mathf.Pow(distanceToPeak, voronoiDropOff) * voronoiFallOff;
+                                break;
+                            case VoronoiType.Linear:
+                                h = peak.y - distanceToPeak * voronoiFallOff;
+                                break;
+                            case VoronoiType.Round:
+                                h = peak.y - Mathf.Pow(distanceToPeak * 3, voronoiFallOff) -
+                                    Mathf.Sin(distanceToPeak * 2 * Mathf.PI) / voronoiDropOff;
+                                break;
+                        }
+
+                        if (heightMap[x, y] < h )
+                            heightMap[x, y] = h;
+                    }
+                }
+            }
+        }
+        terrainData.SetHeights(0, 0, heightMap);
+    }
+    public void MultiplePerlinTerrain()
+    {
+        float[,] heightMap = GetHeightMap();
+
+        for (int x = 0; x < terrainData.heightmapWidth; x++)
+        {
+            for (int y = 0; y < terrainData.heightmapHeight; y++)
+            {
+                foreach(PerlinParameters p in perlinParameters)
+                {
+                    heightMap[x, y] += Utils.fBM(
+                    (x + p.XOffset) * p.XScale,
+                    (y + p.YOffset) * p.YScale,
+                    p.octaves,
+                    p.persistance) * p.heightScale;
+                }
+            }
+        }
+        terrainData.SetHeights(0, 0, heightMap);
+    }
+    public void AddNewPerlin()
+    {
+        perlinParameters.Add(new PerlinParameters());
+    }
+    public void RemovePerlin()
+    {
+        List<PerlinParameters> keptPerlinParameters = new List<PerlinParameters>();
+        for (int i = 0; i< perlinParameters.Count;i++) // why not foreach?
+        {
+            if (!perlinParameters[i].remove)
+            {
+                keptPerlinParameters.Add(perlinParameters[i]);
+            }
+        }
+        if (keptPerlinParameters.Count == 0) // why not check first?
+        {
+            keptPerlinParameters.Add(perlinParameters[0]);
+        }
+        perlinParameters = keptPerlinParameters;
+    }
+    public void Perlin()
+    {
+        float[,] heightMap = GetHeightMap();
+
+        for (int x = 0; x < terrainData.heightmapWidth; x++)
+        {
+            for (int y = 0; y < terrainData.heightmapHeight; y++)
+            {
+                heightMap[x, y] += Utils.fBM(
+                    (x + perlinXOffset) * perlinXScale,
+                    (y + perlinYOffset) * perlinYScale,
+                    perlinOctaves,
+                    perlinPersistance) * perlinHeightScale;
+            }
+        }
+        terrainData.SetHeights(0, 0, heightMap);
+    }
+    public void PerlinSingle()
+    {
+        float[,] heightMap = GetHeightMap();
+
+        for (int x = 0; x < terrainData.heightmapWidth; x++)
+        {
+            for (int y = 0; y < terrainData.heightmapHeight; y++)
+            {
+                heightMap[x, y] += Mathf.PerlinNoise(
+                    (x + perlinXOffset) * perlinXScale,
+                    (y + perlinYOffset) * perlinYScale
+                );
+            }
+        }
+        terrainData.SetHeights(0, 0, heightMap);
+    }
+    public void LoadTexture()
+    {
+        float[,] heightMap = GetHeightMap();
+        for (int x = 0; x < terrainData.heightmapWidth; x++)
+        {
+            for (int y = 0; y < terrainData.heightmapHeight; y++)
+            {
+                heightMap[x, y] += heightMapImage.GetPixel(
+                    (int)(x * heightMapScale.x),
+                    (int)(y * heightMapScale.z)).grayscale * heightMapScale.y;
+            }
+        }
+        terrainData.SetHeights(0, 0, heightMap);
+    }
+    public void ResetTerrain()
+    {
+        float[,] heightMap;
+        heightMap = new float[terrainData.heightmapWidth, terrainData.heightmapHeight];
+        terrainData.SetHeights(0, 0, heightMap);
+    }
+    public void RandomTerrain()
+    {
+        float[,] heightMap = GetHeightMap();
+
+        for (int x = 0; x < terrainData.heightmapWidth; x++)
+        {
+            for (int y = 0; y < terrainData.heightmapHeight; y++)
+            {
+                heightMap[x, y] += UnityEngine.Random.Range(randomHeightRange.x, randomHeightRange.y);
+            }
+        }
+        terrainData.SetHeights(0, 0, heightMap);
+    }
+    private void OnEnable()
+    {
+        Debug.Log("Initializing Terrain Data");
+        terrain = this.GetComponent<Terrain>();
+        terrainData = Terrain.activeTerrain.terrainData;
+    }
+    private void Awake()
+    {
+        SerializedObject tagManager = new SerializedObject(
+            AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]);
+        SerializedProperty tagsProp = tagManager.FindProperty("tags");
+        
+        AddTag(tagsProp, "Terrain");
+        AddTag(tagsProp, "Cloud");
+        AddTag(tagsProp, "Shore");
+
+        tagManager.ApplyModifiedProperties();
+
+        this.gameObject.tag = "Terrain";
+    }
+
+    void AddTag(SerializedProperty tagsProp, string newTag)
+    {
+        bool found = false;
+        for (int i = 0; i < tagsProp.arraySize; i++)
+        {
+            SerializedProperty t = tagsProp.GetArrayElementAtIndex(i);
+            if (t.stringValue.Equals(newTag)) { found = true;  break; }
+        }
+        if (!found)
+        {
+            tagsProp.InsertArrayElementAtIndex(0);
+            SerializedProperty newTagProp = tagsProp.GetArrayElementAtIndex(0);
+            newTagProp.stringValue = newTag;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Editor/CustomTerrainEditor.cs
@@ -0,0 +1,206 @@
+﻿using UnityEngine;
+using UnityEditor;
+using EditorGUITable;
+
+[CustomEditor(typeof(CustomTerrain))] // [1] Links editor to object so when inspecting object, it loads this editor (S02L09) 11:26
+[CanEditMultipleObjects]
+
+
+// Q:
+// - what exactly is serializedObject and how is it linked to the CustomTerrain?
+//
+// GUILayout.Space(20)
+
+public class CustomTerrainEditor : Editor {
+    // properties -----
+    SerializedProperty resetTerrain;
+
+    SerializedProperty randomHeightRange;
+    SerializedProperty heightMapScale;
+    SerializedProperty heightMapImage;
+    SerializedProperty perlinXScale;
+    SerializedProperty perlinYScale;
+    SerializedProperty perlinXOffset;
+    SerializedProperty perlinYOffset;
+
+    SerializedProperty perlinOctaves;
+    SerializedProperty perlinPersistance;
+    SerializedProperty perlinHeightScale;
+
+    GUITableState perlinParameterTable;
+    SerializedProperty perlinParameters;
+
+    SerializedProperty voronoiPeaks;
+    SerializedProperty voronoiMinHeight;
+    SerializedProperty voronoiMaxHeight;
+    SerializedProperty voronoiFallOff;
+    SerializedProperty voronoiDropOff;
+    SerializedProperty voronoiType;
+
+    SerializedProperty mpdMinHeight;
+    SerializedProperty mpdMaxHeight;
+    SerializedProperty mpdDampenerPower;
+    SerializedProperty mpdRoughness;
+
+    SerializedProperty smoothAmount;
+
+    // folds out -----
+    bool showRandom = false;
+    bool showLoadHeights = false;
+    bool showPerlin = false;
+    bool showMultiplePerlin = false;
+    bool showVoronoi = false;
+    bool showMPD = false;
+    bool showSmooth = false;
+
+    void OnEnable()
+    {
+        resetTerrain = serializedObject.FindProperty("resetTerrain");
+
+        randomHeightRange = serializedObject.FindProperty("randomHeightRange");
+        heightMapScale = serializedObject.FindProperty("heightMapScale");
+        heightMapImage = serializedObject.FindProperty("heightMapImage");
+        perlinXScale = serializedObject.FindProperty("perlinXScale");
+        perlinYScale = serializedObject.FindProperty("perlinYScale");
+        perlinXOffset = serializedObject.FindProperty("perlinXOffset");
+        perlinYOffset = serializedObject.FindProperty("perlinYOffset");
+        perlinOctaves = serializedObject.FindProperty("perlinOctaves");
+        perlinPersistance = serializedObject.FindProperty("perlinPersistance");
+        perlinHeightScale = serializedObject.FindProperty("perlinHeightScale");
+
+        perlinParameterTable = new GUITableState("perlinParametersTable");
+        perlinParameters = serializedObject.FindProperty("perlinParameters");
+
+        voronoiPeaks = serializedObject.FindProperty("voronoiPeaks");
+        voronoiMinHeight = serializedObject.FindProperty("voronoiMinHeight");
+        voronoiMaxHeight = serializedObject.FindProperty("voronoiMaxHeight");
+        voronoiFallOff = serializedObject.FindProperty("voronoiFallOff");
+        voronoiDropOff = serializedObject.FindProperty("voronoiDropOff");
+        voronoiType = serializedObject.FindProperty("voronoiType");
+
+        mpdMinHeight = serializedObject.FindProperty("mpdMinHeight");
+        mpdMaxHeight = serializedObject.FindProperty("mpdMaxHeight");
+        mpdDampenerPower = serializedObject.FindProperty("mpdDampenerPower");
+        mpdRoughness = serializedObject.FindProperty("mpdRoughness");
+
+        smoothAmount = serializedObject.FindProperty("smoothAmount");
+    }
+
+    // Display loop for the inspector gui
+    public override void OnInspectorGUI() {
+        serializedObject.Update();
+
+        CustomTerrain terrain = (CustomTerrain)target; // `target` is linked to [1] "a link to class"
+        // terrain.randomHeightRange = .. is possible, but we use serialization because otherwise editing
+        // the code would reset the state and we're loosing whatever we setup in the inspect
+        EditorGUILayout.PropertyField(resetTerrain);
+        showRandom = EditorGUILayout.Foldout(showRandom, "Random"); // show foldout toggle
+        if (showRandom)
+        {
+            GUILayout.Label("Set Heights Between Random Values", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(randomHeightRange);
+            if (GUILayout.Button("Random Heights"))
+            {
+                terrain.RandomTerrain();
+            }
+        }
+        showLoadHeights = EditorGUILayout.Foldout(showLoadHeights, "Load Heights");
+        if (showLoadHeights)
+        {
+            GUILayout.Label("Load Heights From Texture", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(heightMapImage);
+            EditorGUILayout.PropertyField(heightMapScale);
+            if (GUILayout.Button("Load Texture"))
+            {
+                terrain.LoadTexture();
+            }
+        }
+        showPerlin = EditorGUILayout.Foldout(showPerlin, "Single Perlin Noise"); // show foldout toggle
+        if (showPerlin)
+        {
+            EditorGUILayout.Slider(perlinXScale, 0, 0.1f, new GUIContent("X Scale"));
+            EditorGUILayout.Slider(perlinYScale, 0, 0.1f, new GUIContent("Y Scale"));
+            EditorGUILayout.IntSlider(perlinXOffset, 0, 10000, new GUIContent("Offset X"));
+            EditorGUILayout.IntSlider(perlinYOffset, 0, 10000, new GUIContent("Offset Y"));
+            EditorGUILayout.IntSlider(perlinOctaves, 1, 10, new GUIContent("Octaves"));
+            EditorGUILayout.Slider(perlinPersistance, 0.1f, 10, new GUIContent("Persistance"));
+            EditorGUILayout.Slider(perlinHeightScale, 0, 1, new GUIContent("Height Scale"));
+
+            if (GUILayout.Button("Generate"))
+            {
+                terrain.Perlin();
+            }
+        }
+        showMultiplePerlin = EditorGUILayout.Foldout(showMultiplePerlin, "Multple Perlin Noise"); // show foldout toggle
+        if (showMultiplePerlin) {
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+            perlinParameterTable = GUITableLayout.DrawTable(perlinParameterTable,
+                serializedObject.FindProperty("perlinParameters")); // why not the class attribute we mapped?
+            GUILayout.Space(40);
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button("+"))
+            {
+                terrain.AddNewPerlin();
+            }
+            if (GUILayout.Button("-"))
+            {
+                terrain.RemovePerlin();
+            }
+            EditorGUILayout.EndHorizontal();
+            if (GUILayout.Button("Apply Multiple Perlin"))
+            {
+                terrain.MultiplePerlinTerrain();
+            }
+        }
+        showVoronoi = EditorGUILayout.Foldout(showVoronoi, "Voronoi");
+        if (showVoronoi)
+        {
+            EditorGUILayout.IntSlider(voronoiPeaks, 1, 10, new GUIContent("Peak Count"));
+            EditorGUILayout.Slider(voronoiFallOff, 0, 10, new GUIContent("Falloff"));
+            EditorGUILayout.Slider(voronoiDropOff, 0, 10, new GUIContent("Dropoff"));
+            EditorGUILayout.Slider(voronoiMinHeight, 0, 1, new GUIContent("Min Height"));
+            EditorGUILayout.Slider(voronoiMaxHeight, 0, 1, new GUIContent("Max Height"));
+            EditorGUILayout.PropertyField(voronoiType);
+
+            if (GUILayout.Button("Voronoi"))
+            {
+                terrain.Voronoi();
+            }
+        }
+        showMPD = EditorGUILayout.Foldout(showMPD, "Midpoint Displacement");
+        if (showMPD)
+        {
+            EditorGUILayout.PropertyField(mpdMinHeight);
+            EditorGUILayout.PropertyField(mpdMaxHeight);
+            EditorGUILayout.PropertyField(mpdDampenerPower);
+            EditorGUILayout.PropertyField(mpdRoughness);
+
+            if (GUILayout.Button("MPD"))
+            {
+                terrain.MidPointDisplacement();
+            }
+        }
+        showSmooth = EditorGUILayout.Foldout(showSmooth, "Smooth");
+        if (showSmooth)
+        {
+            EditorGUILayout.IntSlider(smoothAmount, 1, 10, new GUIContent("Smooth amount"));
+            if (GUILayout.Button("Smooth"))
+            {
+                terrain.SmoothN();
+            }
+        }
+
+
+        serializedObject.ApplyModifiedProperties();
+    }
+   
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
